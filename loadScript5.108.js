49d5fb5c7e92484d236a8f0e5423554537924a8c
jashjasani <jashjjj3@gmail.com>
2024-02-16 17:31:15 +0530

    temp

--- loadScript5.108.js
+++ loadScript5.108.js
@@ -91,7 +91,7 @@ async function renderData(data) {
       dropdown.addEventListener("click",(event)=>{
         event.preventDefault() // to stop link element from redirecting
         
-        if(document.querySelector("#container-main")!=null) document.querySelector("#container-main").remove()
+        if(document.querySelector("#container-main")!=null && event.target.className != "container-mode") document.querySelector("#container-main").remove()
         
         if(event.target.parentElement.querySelector(".container-mode")==null){
           
