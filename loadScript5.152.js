4af41e431fd1f234ec37b456ddab01a5325927da
jashjasani <jashjjj3@gmail.com>
2024-02-20 11:41:54 +0530

    temp

--- loadScript5.152.js
+++ loadScript5.152.js
@@ -189,6 +189,12 @@ async function renderData(data) {
                         document.querySelectorAll(".btn-specihern.left-btn").forEach((e)=>{
                           e.innerText = event.target.parentElement.childNodes[0].innerText
                         })
+
+                        if(collections.some(obj => obj.items.includes(event.target.parentElement.parentElement.getAttribute("dropdown-key")))){
+                          event.target.parentElement.parentElement.parentElement.querySelector(".btn-specihern").innerText = "Gerettet"
+                        } else {
+                          event.target.parentElement.parentElement.parentElement.querySelector(".btn-specihern").innerText = "Specihern"
+                        }
                       }
                     })
                     
