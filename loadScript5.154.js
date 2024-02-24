15515b0e95c88b47d582c26909f48157841fb306
jashjasani <jashjjj3@gmail.com>
2024-02-25 00:42:32 +0530

    temp

--- loadScript5.154.js
+++ loadScript5.154.js
@@ -164,7 +164,7 @@ async function renderData(data) {
 
                   // save an item to collection
                   if(event.target.innerText == "+"){
-                    let arry = collections.find(obj => obj.name == event.target.parentElement.childNodes[0].innerText)
+                    var arry = collections.find(obj => obj.name == event.target.parentElement.childNodes[0].innerText)
                     
                     fetch("https://bildzeitschrift.netlify.app/.netlify/functions/collection",{
                       method : "PUT",
