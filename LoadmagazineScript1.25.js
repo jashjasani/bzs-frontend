857b456f1885e675732be9599c8f803276af8e75
jashjasani <jashjjj3@gmail.com>
2024-03-01 15:17:06 +0530

    temp

--- LoadmagazineScript1.25.js
+++ LoadmagazineScript1.25.js
@@ -92,6 +92,7 @@ document.addEventListener("DOMContentLoaded", async function () {
             for (let i=0;i<window.collections.length;i++) {
                 console.log(window.collections[i].items,window.productId,window.collections[i].items.includes(window.productId));
                 let includes = window.collections[i].items.includes(window.productId) ? "Gerettet" : "Specihern";
+                console.log(includes);
                 str += `
                         <div style="display:flex; justify-content: space-between;">
                             <div style="margin: 10px;">${window.collections[i].name}</div>
