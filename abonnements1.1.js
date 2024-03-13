f3eedc1bd819c9add1574b704775ae97b10e2571
jashjasani <jashjjj3@gmail.com>
2024-03-13 13:05:13 +0530

    temp

--- abonnements1.1.js
+++ abonnements1.1.js
@@ -194,6 +194,7 @@ function renderPlans(plans, is_active,sub_id){
         {name : "Starter", price : 5, active : false, description : "Das Starter Abo erlaubt es dir, den Filter zur Gänze zu nutzen und somit das Archiv bis ins letzte Details filtern zu können.", price_id : "price_1OsJmuSA2e71Dz91jqdMYH0V" }, 
         {name : "Inspiration", price : 8 , active : false , description : "In diesem Abo hast du einerseits die Möglichkeit, den Filter zur Gänze zu nutzen und andererseits deine eigenen Kollektionen von Magazinen zu speichern. Deine Kollektionen kannst du dann auch in einem Präsentationsmodus abspielen.", price_id : "price_1OqG9PSA2e71Dz91HaJFV0xb"}
     ]
+    Swal.showLoading()
     let result  = await fetch("https://bildzeitschrift.netlify.app/.netlify/functions/subscription", {
         method: "GET",
         headers: {
@@ -201,7 +202,7 @@ function renderPlans(plans, is_active,sub_id){
         },
     })
     if(result.ok){
-
+        
         const active_plan = await result.json()
         if(active_plan.plan){
             const plan = active_plan.plan
@@ -224,4 +225,5 @@ function renderPlans(plans, is_active,sub_id){
         }
 
     }
+    Swal.close()
 })().then()
