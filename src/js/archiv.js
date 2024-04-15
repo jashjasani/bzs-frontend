cd0ed2cd58429e42567cd10c71bfa45e85ac690e
jashjasani <jashjjj3@gmail.com>
2024-04-15 17:36:34 +0530

    fix tag wrap

--- src/js/archiv.js
+++ src/js/archiv.js
@@ -687,9 +687,11 @@ async function loadFData(e) {
   }
   setTimeout(async () => {
     await (async () => {
-      document.querySelectorAll(".tag_wrap")[document.querySelectorAll(".tag_wrap").length-1].addEventListener("click", ()=>{
-        loadFData()
-      })
+      if(document.querySelectorAll(".tag_wrap")){
+        document.querySelectorAll(".tag_wrap")[document.querySelectorAll(".tag_wrap").length-1].addEventListener("click", ()=>{
+          loadFData()
+        })
+      }
       const url = window.location.href;
       const getQuery = url.split("?")[1];
       let queryCookie = "";
